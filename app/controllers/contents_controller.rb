3b6a8ca3559a3359ad7e0ab18a9e182ddfd9fdef
Atp <servizio-edp@atp.it>
2023-02-15 16:19:09 +0100

    Scaffold - Contents (pt.1)

---
+++ app/controllers/contents_controller.rb
@@ -0,0 +1,57 @@
+class ContentsController < ApplicationController
+  before_action :set_content, only: %i[ show edit update destroy ]
+
+  def index
+    @contents = Content.all
+  end
+
+  def show
+  end
+
+  def new
+    @content = Content.new
+  end
+
+  def edit
+  end
+
+  def create
+    @content = Content.new(content_params)
+
+    respond_to do |format|
+      if @content.save
+        format.html { redirect_to content_url(@content), notice: "Content was successfully created." }     
+      else
+        format.html { render :new, status: :unprocessable_entity }  
+      end
+    end
+  end
+
+  def update  
+    respond_to do |format|
+      if @content.update(content_params)
+        format.html { redirect_to content_url(@content), notice: "Content was successfully updated." }
+      else
+        format.html { render :edit, status: :unprocessable_entity }   
+      end
+    end
+  end
+
+  def destroy
+    @content.destroy
+
+    respond_to do |format|
+      format.html { redirect_to contents_url, notice: "Content was successfully destroyed." }
+    end
+  end
+
+  private
+
+    def set_content
+      @content = Content.find(params[:id])
+    end
+
+    def content_params
+      params.require(:content).permit(:titolo, :descrizione, :price)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+
+  resources :contents
+
   devise_for :users
+  
   root 'pages#home'
 
   get 'about' => 'pages#about'
