1ad95771d94f51d85842a44c2fce7b9ea5d9f85c
Atp <servizio-edp@atp.it>
2023-03-03 14:59:46 +0100

    aggiunta dell'allegato non visualizzabile

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -55,7 +55,7 @@ class ContentsController < ApplicationController
     end
 
     def content_params
-      params.require(:content).permit(:titolo, :descrizione, :price, :cover)
+      params.require(:content).permit(:titolo, :descrizione, :price, :cover, :contenuto)
     end
 
     def check_user
--- app/models/content.rb
+++ app/models/content.rb
@@ -2,4 +2,5 @@ class Content < ApplicationRecord
 	belongs_to :user
 
 	has_one_attached :cover
+	has_one_attached :contenuto
 end
