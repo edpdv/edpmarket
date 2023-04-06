6d5e38bf24a5f7edc604f8a02a6905435e68f936
Atp <servizio-edp@atp.it>
2023-04-06 09:50:22 +0200

    Validazione dei campi da compilare per i contenuti

--- app/models/content.rb
+++ app/models/content.rb
@@ -3,4 +3,10 @@ class Content < ApplicationRecord
 
 	has_one_attached :cover
 	has_one_attached :contenuto
+
+	validates :titolo, :descrizione, :price, presence: true
+	validates :price, numericality: { greater_than: 0 }
+	validates :cover, presence: true
+	validates :contenuto, presence: true
+
 end
